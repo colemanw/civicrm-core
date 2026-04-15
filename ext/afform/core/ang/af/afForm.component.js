66e7d8ada3da6ef014484bb58007e30b64e02603
colemanw <coleman@civicrm.org>
2026-04-15 13:21:21 -0400

    Afform - Fix error in displayError

--- ext/afform/core/ang/af/afForm.component.js
+++ ext/afform/core/ang/af/afForm.component.js
@@ -347,15 +347,15 @@
         displayError(errorMsg, ts('Sorry'), 'error');
       }
 
-     function displayError(error, title, type) {
+     function displayError(message, title, type) {
         if (typeof Swal === 'function') {
           Swal.fire({
             icon: type,
-            html: error.error_message.replace("\n", '<br>')
+            html: message.replace("\n", '<br>')
           });
         }
         else {
-          CRM.alert(error.error_message, title, type);
+          CRM.alert(message, title, type);
         }
       }
 
